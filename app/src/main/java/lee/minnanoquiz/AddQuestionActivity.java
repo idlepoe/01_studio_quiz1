b91b764fcf368c34d7f0e722acc2bd132a719b18
idlepoe <idlepoe@gmail.com>
2018-12-22 22:24:13 +0900

    test3

--- app/src/main/java/lee/minnanoquiz/AddQuestionActivity.java
+++ app/src/main/java/lee/minnanoquiz/AddQuestionActivity.java
@@ -2,12 +2,24 @@ package lee.minnanoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddQuestionActivity extends AppCompatActivity {
+
+    EditText editWord;
+    EditText editPronounce;
+    EditText editMeaning;
+    EditText editComment;
+
+    Button btnAddQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+
+        editWord = findViewById(R.id.editWord);
+        editPronounce = findViewById(R.id.editPronounce);
     }
 }
